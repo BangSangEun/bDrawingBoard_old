f6714a841d77fd45d3449a554910ffab7a964e69
sebang <sebang@hancom.com>
2016-11-07 17:51:21 +0900

    - 두번째 테스트파일 입니다.

--- src/main/webapp/js/Initializer.js
+++ src/main/webapp/js/Initializer.js
@@ -24,7 +24,7 @@ define(['jquery', 'Tool', 'Util', 'DrawingEventHandler', 'FileEventHandler'],
             };
 
             /**
-             * 이벤트 바인딩as
+             * 이벤트 바인딩asaaaasdf
              */
             this.eventBinder = function() {
                     //======================================= file
